2056faedea97afd2d765a58865657a365d9836ed
Neroxgd <darkaux69@gmail.com>
2024-11-21 18:51:35 +0100

    movement basic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,25 @@ using DG.Tweening;
 public class Player : MonoBehaviour
 {
     private Membre[] membres;
-    private Tween[] membretween;
 
     private void Start()
     {
         membres = GetComponentsInChildren<Membre>();
-        membretween = new Tween[membres.Length];
     }
 
     public void ButtonAction(InputAction.CallbackContext context)
     {
         if (context.started)
             for (int i = 0; i < membres.Length; i++)
-                membretween[i] = membres[i].transform.DOBlendableLocalRotateBy(membres[i].FinalPosition, membres[i].Speed).SetAutoKill(false);
+            {
+                DOTween.Kill(membres[i].transform);
+                membres[i].transform.DOLocalRotate(membres[i].FinalPosition, membres[i].Speed).SetSpeedBased(true);
+            }
         else if (context.canceled)
             for (int i = 0; i < membres.Length; i++)
-                membretween[i].PlayBackwards();
+            {
+                DOTween.Kill(membres[i].transform);
+                membres[i].transform.DOLocalRotate(Vector3.zero, membres[i].Speed).SetSpeedBased(true);
+            }
     }
 }
